2c8039c04d1494f218b257c99b16618058e3f53e
Mati-Rosental <matiasrosental97@gmail.com>
2026-06-04 22:24:44 -0300

    Productos en main
    Funcion de gestion de productos integrada al main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from login import iniciar_sesion
 from clientes import crear_cliente, listar_clientes, modificar_cliente, baja_cliente, buscar_clientes
 from empleados import crear_empleado, listar_empleados, modificar_empleado, baja_empleado, buscar_empleados
 from calculadora import calculadora_corporal
+from productos import crear_producto, listar_productos, modificar_producto, baja_producto, buscar_productos
 
 
 def menu_clientes():
@@ -68,6 +69,37 @@ def menu_empleados():
             print("Opcion invalida.")
 
 
+def menu_productos():
+    opcion = ""
+    while opcion != "0":
+        print("\n" + "=" * 40)
+        print("GESTION DE PRODUCTOS")
+        print("=" * 40)
+        print("1. Alta de producto")
+        print("2. Listar productos activos")
+        print("3. Buscar producto")
+        print("4. Modificar producto")
+        print("5. Baja de producto")
+        print("0. Volver al menu principal")
+        
+        opcion = input("\nOpcion: ")
+        
+        if opcion == "1":
+            crear_producto()
+        elif opcion == "2":
+            listar_productos()
+        elif opcion == "3":
+            buscar_productos()
+        elif opcion == "4":
+            modificar_producto()
+        elif opcion == "5":
+            baja_producto()
+        elif opcion == "0":
+            return
+        else:
+            print("Opcion invalida.")
+
+
 def menu_principal():
     opcion = ""
     while opcion != "0":
@@ -76,7 +108,8 @@ def menu_principal():
         print("=" * 40)
         print("1. Gestion de Clientes")
         print("2. Gestion de Empleados")
-        print("3. Calculadora corporal")
+        print("3. Gestion de Productos")
+        print("4. Calculadora corporal")
         print("0. Salir")
         
         opcion = input("\nOpcion: ")
@@ -86,6 +119,8 @@ def menu_principal():
         elif opcion == "2":
             menu_empleados()
         elif opcion == "3":
+            menu_productos()
+        elif opcion == "4":
             calculadora_corporal()
         elif opcion == "0":
             print("\nSaliendo del sistema...")
